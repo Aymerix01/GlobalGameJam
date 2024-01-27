a3a8dcb7d3ba2aecc0ed14ad9c19165259832dad
Jeremy-afk <jeremylenoir2002@gmail.com>
2024-01-27 12:52:45 +0100

    Card Prefab
    The card script is where you customize everything

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Card : MonoBehaviour
+{
+    [Header("Top")]
+    [SerializeField] private int pA;
+
+    enum DataType { Attack, Utility }
+    [SerializeField] DataType type;
+
+    [Header("Image")]
+    [SerializeField] private Sprite cardImage;
+
+    [TextArea]
+    [Header("Description")]
+    [SerializeField] private string joke;
+
+    [TextArea]
+    [SerializeField] private string sucess;
+
+    [TextArea]
+    [SerializeField] private string fail;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReplaceEverything();
+    }
+
+    void ReplaceEverything()
+    {
+        //Top
+        //PA
+        transform.GetChild(3).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = pA + " PA";
+        //Type
+        if (type == DataType.Attack)
+        {
+            //top right sprite
+            transform.GetChild(3).GetChild(0).GetChild(1).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Attack");
+            //background top
+            transform.GetChild(3).gameObject.GetComponent<SpriteRenderer>().color = new Color(0.6588235f, 0.1994776f, 0.1199059f, 1f);
+            //background middle
+            transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = new Color(0.909434f, 0.6994405f, 0.5473763f, 1f);
+            //background bottom
+            transform.GetChild(2).gameObject.GetComponent<SpriteRenderer>().color = new Color(0.8039216f, 0.3721133f, 0.2784314f, 1f);
+        }
+        if (type == DataType.Utility)
+        {
+            //top right sprite
+            transform.GetChild(3).GetChild(0).GetChild(1).gameObject.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Utility");
+            //background top
+            transform.GetChild(3).gameObject.GetComponent<SpriteRenderer>().color = new Color(0.300383f, 0.6588235f, 0.1215686f, 1f);
+            //background middle
+            transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = new Color(0.7683073f, 0.9098039f, 0.5490196f, 1f);
+            //background bottom
+            transform.GetChild(2).gameObject.GetComponent<SpriteRenderer>().color = new Color(0.6163785f, 0.8037735f, 0.2775293f, 1f);
+        }
+
+        //Image
+        transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().sprite = cardImage;
+
+        //Description
+        //Joke
+        transform.GetChild(2).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = joke;
+        //Sucess
+        transform.GetChild(2).GetChild(0).GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = "Rire : " + sucess;
+        //Fail
+        transform.GetChild(2).GetChild(0).GetChild(2).gameObject.GetComponent<TextMeshProUGUI>().text = "Bad : " + fail;
+    }
+}
