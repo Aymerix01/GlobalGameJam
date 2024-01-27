b1ff1496349711fba9d912787947e3ff8f68ed7b
Aymeric <aymerix.lacroix@gmail.com>
2024-01-27 15:34:33 +0100

    UI

---
+++ Assets/Scripts/UI/DeckController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
+
+public class DeckController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Cards = new GameObject[12];
+    [SerializeField] private Transform[] CardPos = new Transform[4];
+    [SerializeField] private GameObject CardBack;
+
+    private DragDrop dragDrop;
+    private GameObject CardBackSpawned;
+    private List<DragDrop> CardsInHand;
+    private List<DragDrop> CardsInDeck;
+    private void Awake()
+    {
+
+        CardsInHand = new List<DragDrop>();
+        CardsInDeck = new List<DragDrop>();
+        int i = 0;
+        foreach (GameObject card in Cards)
+        {
+            if (card != null)
+            {
+                CardsInDeck.Add(Instantiate(card).GetComponent<DragDrop>());
+                CardsInDeck[i].transform.position = transform.position;
+            }
+            i++;
+        }
+        foreach (Transform cardPos in CardPos)
+        {
+            int randomCard = Random.Range(0, CardsInDeck.Count);
+            CardsInDeck[randomCard].transform.position = cardPos.position;
+            CardsInDeck[randomCard].IsDraggable = true;
+            CardsInHand.Add(CardsInDeck[randomCard]);
+            CardsInDeck.RemoveAt(randomCard);
+        }
+        CardBackSpawned = Instantiate(CardBack, transform);
+    }
+
+    public void UpdateCardPlayable(Vector3 pos)
+    {
+        if (CardsInDeck.Count > 0)
+        {
+            int randomCard = Random.Range(0, CardsInDeck.Count);
+            CardsInDeck[randomCard].transform.position = pos;
+            CardsInDeck[randomCard].InitialPosition = pos;
+            CardsInDeck[randomCard].IsDraggable = true;
+            CardsInHand.Add(CardsInDeck[randomCard]);
+            CardsInDeck.RemoveAt(randomCard);
+        }
+        else
+        {
+            Destroy(CardBackSpawned);
+        }
+    }
+
+    public void RemoveCardInHand(DragDrop dd)
+    {
+        CardsInHand.Remove(dd);
+    }
+
+    public void RerollOneCard(DragDrop dd, Vector3 pos)
+    {
+        Debug.Log("Reroll");
+        CardsInDeck.Add(dd);
+        CardsInDeck[CardsInDeck.Count-1].transform.position = transform.position;
+        CardsInDeck[CardsInDeck.Count - 1].IsDraggable = false;
+        UpdateCardPlayable(pos);
+    }
+
+    public void RerollAllCards()
+    {
+        int i = 0;
+        CardsInHand.RemoveAll(x => !x);
+        if (CardsInHand.Count > 0)
+        {
+            Debug.Log(CardsInHand.Count);
+            foreach (DragDrop card in CardsInHand.ToArray())
+            {
+                if (i < 4)
+                {
+                    RerollOneCard(card, CardPos[i].transform.position);
+                }
+                i++;
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/DragDrop.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour
+{
+    private Vector3 mousePositionOffset;
+    private Camera camera;
+    public Vector3 InitialPosition;
+    public bool IsDraggable;
+    private bool HasPlayThisCard;
+    private bool HasReroll;
+    private DeckController deckController;
+
+    private void Start()
+    {
+        camera = Camera.main;
+        deckController = GameObject.Find("PosDeck").GetComponent<DeckController>();
+        InitialPosition = transform.position;
+        HasPlayThisCard = false;
+    }
+
+    private Vector3 GetMouseWorldposition()
+    {
+        return camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void ReturnToInitialPosition()
+    {
+        transform.position = InitialPosition;
+    }
+
+    private void OnMouseDown()
+    {
+        mousePositionOffset = transform.position - GetMouseWorldposition();
+    }
+
+    private void OnMouseDrag()
+    {
+        if (IsDraggable)
+        {
+            transform.position = GetMouseWorldposition() + mousePositionOffset;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "DropZone")
+        {
+            HasPlayThisCard = true;
+        }
+        else if (other.gameObject.tag == "Reroll")
+        {
+            HasReroll = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "DropZone")
+        {
+            HasPlayThisCard = false;
+        }
+        else if (other.gameObject.tag == "Reroll")
+        {
+            HasReroll = false;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (IsDraggable)
+        {
+            if (HasPlayThisCard)
+            {
+                deckController.UpdateCardPlayable(InitialPosition);
+                Debug.Log("Here to start effect of this card : "+gameObject.name);
+                Destroy(gameObject);
+                deckController.RemoveCardInHand(this);
+            }
+            else if (HasReroll)
+            {
+                deckController.RerollOneCard(this, InitialPosition);
+            }
+            else
+            {
+                ReturnToInitialPosition();
+            }
+        }
+    }
+}
